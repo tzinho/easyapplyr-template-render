27994abbdbc2638407207b54e5459b5e12db3b11
tzinho.indie <tzinho.indie@gmail.com>
2025-01-14 17:41:00 -0300

    initial commit

--- src/server/api/routers/template.ts
+++ src/server/api/routers/template.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { posts } from "~/server/db/schema";
+import { templates } from "~/server/db/schema";
 
-export const postRouter = createTRPCRouter({
+export const templateRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
@@ -15,16 +15,8 @@ export const postRouter = createTRPCRouter({
   create: publicProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
-      await ctx.db.insert(posts).values({
+      await ctx.db.insert(templates).values({
         name: input.name,
       });
-    }),
-
-  getLatest: publicProcedure.query(async ({ ctx }) => {
-    const post = await ctx.db.query.posts.findFirst({
-      orderBy: (posts, { desc }) => [desc(posts.createdAt)],
-    });
-
-    return post ?? null;
-  }),
+    })
 });
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,11 +1,13 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import {
+  boolean,
   index,
   integer,
   pgTableCreator,
+  text,
   timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
@@ -18,19 +20,235 @@ import {
  */
 export const createTable = pgTableCreator((name) => `easyapplyr-template-render_${name}`);
 
-export const posts = createTable(
-  "post",
-  {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+export const resumes = createTable('resume', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  title: text('title').notNull(),
+})
+
+export const contacts = createTable('contact', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  resumeId: text('resumeId')
+    .notNull()
+    .references(() => resumes.id, { onDelete: 'cascade' }),
+  name: text('name'),
+  email: text('email'),
+  phone: text('phone'),
+  linkedin: text('linkedin'),
+  personal: text('personal'),
+  country: text('country'),
+  state: text('state'),
+  city: text('city'),
+  column: integer('column'),
+  order: integer('order'),
+})
+
+export const contactsRelations = relations(contacts, ({ one }) => ({
+  resume: one(resumes, {
+    fields: [contacts.resumeId],
+    references: [resumes.id],
+  }),
+}))
+
+export const experiences = createTable('experience', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  resumeId: text('resumeId')
+    .notNull()
+    .references(() => resumes.id, { onDelete: 'cascade' }),
+  role: text('role'),
+  company: text('company'),
+  where: text('where'),
+  did: text('did'),
+  appear: boolean('appear'),
+  column: integer('column'),
+  order: integer('order'),
+})
+
+export const experiencesRelations = relations(experiences, ({ one }) => ({
+  resume: one(resumes, {
+    fields: [experiences.resumeId],
+    references: [resumes.id],
+  }),
+}))
+
+export const projects = createTable('project', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  resumeId: text('resumeId')
+    .notNull()
+    .references(() => resumes.id, { onDelete: 'cascade' }),
+  appear: boolean('appear'),
+  column: integer('column'),
+  order: integer('order'),
+})
+
+export const projectsRelations = relations(projects, ({ one }) => ({
+  resume: one(resumes, {
+    fields: [projects.resumeId],
+    references: [resumes.id],
+  }),
+}))
+
+export const educations = createTable('education', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  resumeId: text('resumeId')
+    .notNull()
+    .references(() => resumes.id, { onDelete: 'cascade' }),
+  appear: boolean('appear'),
+  column: integer('column'),
+  order: integer('order'),
+})
+
+export const educationsRelations = relations(educations, ({ one }) => ({
+  resume: one(resumes, {
+    fields: [educations.resumeId],
+    references: [resumes.id],
+  }),
+}))
+
+export const certifications = createTable('certification', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  resumeId: text('resumeId')
+    .notNull()
+    .references(() => resumes.id, { onDelete: 'cascade' }),
+  appear: boolean('appear'),
+  column: integer('column'),
+  order: integer('order'),
+})
+
+export const certificationsRelations = relations(certifications, ({ one }) => ({
+  resume: one(resumes, {
+    fields: [certifications.resumeId],
+    references: [resumes.id],
+  }),
+}))
+
+export const courseworks = createTable('coursework', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  resumeId: text('resumeId')
+    .notNull()
+    .references(() => resumes.id, { onDelete: 'cascade' }),
+  appear: boolean('appear'),
+  column: integer('column'),
+  order: integer('order'),
+})
+
+export const courseworkRelations = relations(courseworks, ({ one }) => ({
+  resume: one(resumes, {
+    fields: [courseworks.resumeId],
+    references: [resumes.id],
+  }),
+}))
+
+export const involvements = createTable('involvement', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  resumeId: text('resumeId')
+    .notNull()
+    .references(() => resumes.id, { onDelete: 'cascade' }),
+  role: text('role'),
+  organization: text('organization'),
+  appear: boolean('appear'),
+  column: integer('column'),
+  order: integer('order'),
+})
+
+export const involvementRelations = relations(involvements, ({ one }) => ({
+  resume: one(resumes, {
+    fields: [involvements.resumeId],
+    references: [resumes.id],
+  }),
+}))
+
+export const skills = createTable('skill', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  resumeId: text('resumeId')
+    .notNull()
+    .references(() => resumes.id, { onDelete: 'cascade' }),
+  text: text('text'),
+  appear: boolean('appear'),
+  column: integer('column'),
+  order: integer('order'),
+})
+
+export const skillsRelations = relations(skills, ({ one }) => ({
+  resume: one(resumes, {
+    fields: [skills.resumeId],
+    references: [resumes.id],
+  }),
+}))
+
+export const summaries = createTable('summary', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  resumeId: text('resumeId')
+    .notNull()
+    .references(() => resumes.id, { onDelete: 'cascade' }),
+  summary: text('summary'),
+  appear: boolean('appear'),
+  column: integer('column'),
+  order: integer('order'),
+})
+
+export const summaryRelations = relations(summaries, ({ one }) => ({
+  resume: one(resumes, {
+    fields: [summaries.resumeId],
+    references: [resumes.id],
+  }),
+}))
+
+export const languages = createTable('language', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  resumeId: text('resumeId')
+    .notNull()
+    .references(() => resumes.id, { onDelete: 'cascade' }),
+  name: text('name').notNull(),
+  appear: boolean('appear'),
+  column: integer('column'),
+  order: integer('order'),
+})
+
+export const languageRelations = relations(languages, ({ one }) => ({
+  resume: one(resumes, {
+    fields: [languages.resumeId],
+    references: [resumes.id],
+  }),
+}))
+
+export const resumeRelations = relations(resumes, ({ many, one }) => ({
+  experiences: many(experiences),
+  projects: many(projects),
+  educations: many(educations),
+  certifications: many(certifications),
+  courseworks: many(courseworks),
+  involvements: many(involvements),
+  skills: many(skills),
+  contact: one(contacts),
+  languages: many(languages),
+}))
+
+export const templates = createTable('templates', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  name: text('name'),
+})
+
