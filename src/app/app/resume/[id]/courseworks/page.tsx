1f564b6cd385934801e7ac13e6b137b53f3ce016
Thiago Luiz <thiagoluiz@MacBook-Pro-de-Vitoria.local>
2025-02-13 23:21:23 -0300

    initial commit

--- src/app/app/resume/[id]/courseworks/page.tsx
+++ src/app/app/resume/[id]/courseworks/page.tsx
@@ -1,13 +1,9 @@
-import { type Resume } from "~/types/template";
-import { api } from "~/trpc/server";
-
 interface CourseworksProps {
   params: Promise<{ id: string }>;
 }
 
 const Courseworks = async ({ params }: CourseworksProps) => {
   const { id } = await params;
-  const data = (await api.resumes.get(id)) as Resume;
   return null;
 };
 
